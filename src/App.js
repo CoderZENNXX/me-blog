b859c8616f45aa748ad0071d59751801caf9a141
CoderZENNXX <brocodestudent@gmail.com>
2024-12-03 17:18:30 +0700

    to part 4 (all features)

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const googleLink = "https://www.google.com";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Components</h1>
+        <h1>
+          {title}
+        </h1>
+
+        <p>
+          Liked {likes} times
+        </p>
+
+        <p>{10}</p>
+        <p>{"hello, ninjas"}</p>
+        <p>{[1,2,3,4,5]}</p>
+        <p>{Math.random() * 10}</p>
+
+        <a href={googleLink}>Google</a>
       </div>
     </div>
   );
