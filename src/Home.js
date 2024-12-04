a410cc353184a09bb56b95d51b89b7024a2178a0
CoderZENNXX <brocodestudent@gmail.com>
2024-12-04 17:09:37 +0700

    to part 12 (all features)

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs"></BlogList>
+            <BlogList blogs={blogs.filter((blog) => blog.author === "Tayzar")} title="Tayzar's Blogs"></BlogList>
         </div>
     );
 }
