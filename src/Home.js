01dc47f7029f5f6bd74fcbb22b38689b92a022ea
CoderZENNXX <brocodestudent@gmail.com>
2024-12-04 14:12:06 +0700

    to part 8 (all features)

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,20 @@
+import {useState} from "react"
+
 const Home = () => {
 
-    const handleClick = (name, e) => {
-        console.log(`Hello ${name}`)
+    const [myName, setMyName] = useState("Tayzar");
+    const [age, setAge] = useState(12);
+
+    const handleClick = () => {
+        setMyName("Myat")
+        setAge(24)
     }
 
     return (
         <div className="home">
             <h2>Home Page</h2>
-            <button onClick={() => handleClick("Tayzar")}>Hello Button</button>
+            <p>{myName} is {age} years old</p>
+            <button onClick={() => handleClick("Tayzar")}>Name Button</button>
         </div>
     );
 }
