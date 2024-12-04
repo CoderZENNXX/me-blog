6bd704f070639ce2e63b2715d2d94e751300c73e
CoderZENNXX <brocodestudent@gmail.com>
2024-12-04 17:03:30 +0700

    to part 11 (all features)

---
+++ src/BlogList.js
@@ -0,0 +1,18 @@
+const BlogList = ({blogs, title}) => {
+
+    return (
+        <div className="blog-list">
+            <h2 style={{ 
+          color: '#f1356d'
+        }}>{title}</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default BlogList;
