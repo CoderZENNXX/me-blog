dda3db8fc5f9054c0049e7c774ba75a9948dd235
CoderZENNXX <brocodestudent@gmail.com>
2024-12-04 13:37:02 +0700

    to part 7

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+
+    const handleClick = (name, e) => {
+        console.log(`Hello ${name}`)
+    }
+
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={() => handleClick("Tayzar")}>Hello Button</button>
         </div>
     );
 }
