c42f927952cef05c3741f5dc316e03959cd0f629
CoderZENNXX <brocodestudent@gmail.com>
2024-12-11 17:16:16 +0700

    to part 19 (all features)

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,30 @@ import BlogList from "./BlogList"
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(null);
 
     useEffect(() => {
         fetch("http://localhost:8000/blogs")
         .then(res => {
+            if (!res.ok) {
+                throw Error("Could not get a response for the fetch!!")
+            }
             return res.json()
         })
         .then(data => {
             setBlogs(data)
             setIsLoading(false)
+            setIsLoading(null)
+        })
+        .catch(err => {
+            setIsLoading(false)
+            setIsError(err.message);
         })
     }, [])
 
     return (
         <div className="home">
+            {isError && <div><h1>Could not get a response for the fetch!!</h1></div>}
             {isLoading && <div><h1>Loading...</h1></div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"></BlogList>}
         </div>
