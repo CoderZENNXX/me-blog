c0eb5533efb1a3175ef90c38344b1766ef2134be
CoderZENNXX <brocodestudent@gmail.com>
2025-01-22 16:37:31 +0700

    part 27 (all features)

--- src/CreateBlog.js
+++ src/CreateBlog.js
@@ -1,9 +1,44 @@
-const createBlog = () => {
+import { useState } from "react";
+
+const CreateBlog = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("");
+
     return (
         <div className="create">
             <h2>Add a new blog</h2>
+            <form>
+                <label>Title:</label>
+                <input 
+                type="text"
+                required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                >
+                </input>
+
+                <label>Text:</label>
+                <textarea
+                required
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                >
+                </textarea>
+
+                <label>Author:</label>
+                <input 
+                type="text"
+                required
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                >
+                </input>
+
+                <button>Add Blog</button>
+            </form>
         </div>
     );
 }
  
-export default createBlog;
+export default CreateBlog;
