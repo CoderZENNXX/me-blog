71501a664a2f6e31031c44ea9274e49732c46ed7
CoderZENNXX <brocodestudent@gmail.com>
2025-01-22 16:08:03 +0700

    part 26 (all features)

--- src/BlogsDetail.js
+++ src/BlogsDetail.js
@@ -1,11 +1,24 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogsDetail = () => {
     const {id} = useParams();
+    const {data: blogs, IsError, IsLoading} = useFetch("http://localhost:8000/blogs/" + id)
 
     return (
     <div className="blogs-detail">
-        <h2>Blogs Detail - {id}</h2>
+        <h2>
+            {IsLoading && <div>Loading....</div>}
+            {IsError && <div>{IsError}</div>}
+            {blogs && (
+                <article>
+                    <h2>{blogs.title}</h2>
+                    <div>{blogs.body}</div>
+                    <p>Written by {blogs.author}</p>
+                </article>
+            )
+            }
+        </h2>
     </div>
     );
 }
