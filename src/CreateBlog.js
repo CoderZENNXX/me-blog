09a212fefbfd535dfbdb8f92b90756d47463a563
CoderZENNXX <brocodestudent@gmail.com>
2025-01-22 17:19:28 +0700

    part 30 (all features)

--- src/CreateBlog.js
+++ src/CreateBlog.js
@@ -1,14 +1,27 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const CreateBlog = () => {
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
     const [author, setAuthor] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const history = useHistory()
 
     const handleSubmit = (e) => {
         e.preventDefault()
         const blog = {title, body, author}
-        console.log(blog)
+
+        fetch("http://localhost:8000/blogs", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        })
+        .then(() => {
+            console.log("New Blog Added")
+            setIsLoading(false)
+            history.push("/")
+        })
     }
 
     return (
@@ -41,7 +54,9 @@ const CreateBlog = () => {
                 >
                 </input>
 
-                <button>Add Blog</button>
+                {!isLoading && <button>Add Blog</button>}
+                {isLoading && <button disabled>Adding Blog....</button>}
+
             </form>
         </div>
     );
